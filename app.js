6f48414ae828f55b594b5f417dbeeec566541d4a
miku <nunon4msmrn@icloud.com>
2023-09-01 14:10:20 +0900

    docs 予約画面作成

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ const connection = mysql.createConnection({
 
 app.set('view engine', 'ejs');
 
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
 
 app.get('/',(req,res)=>{
     res.render('top.ejs');
@@ -49,7 +54,27 @@ app.get('/plans',(req,res)=>{
 app.get('/reservation/:id',(req,res)=>{
     const name = req.params.id;
 
-    res.render('reservation.ejs',{name: name})
+    connection.query(
+        'SELECT * FROM sites WHERE siteName = ?',
+        [name],
+        (error,results)=>{
+            const pplNum = results[0].pplNum;
+            res.render('reservation.ejs',{name: name, pplNum: pplNum})
+        }
+    );
+
+});
+
+app.get('/confirmation',(req,res)=>{
+    // const name = req.body.name;
+    // const tel = req.body.tel;
+    // const site = req.body.site;
+    // const data = req.body.data;
+    // const time = req.body.time;
+    // const pplNum = req.body.pplNum;
+    
+
+    res.render('confirmation.ejs');
 });
 
 
